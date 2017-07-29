626c7028a86b4510f38805d02e565963bcdb6a6c
mathheusbueno <mathheusbueno@gmail.com>
2017-07-29 16:18:06 -0300

    Add files via upload

---
+++ src/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritimeticos;
+
+/**
+ *
+ * @author mathh
+ */
+public class OperadoresAritimeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int m1 = 3;
+        int m2 = 5;
+        float m = (m1 + m2)/2;
+        System.out.println("A media é: " + m);
+        */
+       /* int numero = 5;
+        numero = numero+ + 5;
+        System.out.println("Numero é: "+numero);
+        */
+       double ale = 5+ Math.random()*(5);
+       int num = (int) (0 + Math.random()*(75));
+        System.out.println("Numero:"+num);
+    }
+      
+    }
+    
+
